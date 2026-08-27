bfcdbd6bb2f73a47768d26f64c15ae1d434bbdbf
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-27 12:45:10 +0800

    red: acceptance test against unfixed code

---
+++ internal/model/errchain.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"errors"
+	"fmt"
+)
+
+func DriftChain(pct float64) error {
+	return fmt.Errorf("pulp level %.1f drift exceeded: %w", pct, ErrPulpDrift)
+}
+
+func TripChain(zone string, celsius float64) error {
+	return fmt.Errorf("heat alarm zone %s at %.1f: %w", zone, celsius, ErrLevelTrip)
+}
+
+func HoldChain(err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("gradient hold: %w", err)
+}
+
+func IsDrift(err error) bool { return errors.Is(err, ErrPulpDrift) }
+func IsTrip(err error) bool { return errors.Is(err, ErrLevelTrip) }
+func IsHold(err error) bool { return errors.Is(err, ErrSpargeHold) }
